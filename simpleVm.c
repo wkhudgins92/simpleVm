4060ef541457a5d6f3f24e5ce264a20f5ab0dadf
William Hudgins <will@wkhudgins.info>
2016-07-04 18:57:01 -0500

    Added documentation

--- simpleVm.c
+++ simpleVm.c
@@ -1,44 +1,60 @@
+/*
+ * Implements a simple stack based VM and provides a 7 instruction ISA for the
+ * VM.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>                                                              
 #include <string.h>             
 #include <regex.h>                                                
                                                                                 
-//#define LINE_MAX 128;                                                         
+#define LINE_MAX 128                                                         
+#define STACK_SIZE 100
+#define INSTRUCTION_SET_SIZE 7
+
 int countLines(FILE* infile);                                                   
-int containsInvalidCommand(char commandQueue[][128], int numCommands,
+int containsInvalidCommand(char commandQueue[][LINE_MAX], int numCommands,
 	int* errorLine);
-void executeCode(char commandQueue[][128], int numCommands);
-                                                                                
+void executeCode(char commandQueue[][LINE_MAX], int numCommands);
+
+/**
+ * Main function for this program. 
+ * Expects an accessible file to be passed in argv for execution.
+ */
 int main (int argc, char* argv[]) {                                             
-    FILE* sourceCode;                                                           
+    int i, numLines;                                                               
     char* scriptName;                                                           
-    int numLines;                                                               
-    int i;                                                                      
+    FILE* sourceCode;                                                           
                                                                                 
-                                                                                
+    // Insufficient number of arguments supplied                                                                            
     if (argc != 2) {                                                            
         printf("Usage: simpleVm script.simple\n");                              
         return 1;                                                               
     }                                                                           
-                                                                                
-    scriptName = argv[1];                                                       
+    
+	// Get the name of the script                                                                            
+    scriptName = argv[1];        
+
+	// If file cannot be opened                                               
     if (!(sourceCode = fopen(scriptName, "r"))) {                               
         printf("Error accessing %s\n", scriptName);                             
         return 1;                                                               
     }                                                                           
-                                                                                
+   
+	// Read each command into an array 
     numLines = countLines(sourceCode);                                          
     if (numLines == 0)                                                          
         return 0;                                                               
                                                                                 
     rewind(sourceCode);                                                         
-    char commandQueue[numLines][128];                                           
+    char commandQueue[numLines][LINE_MAX];                                       
     i = 0;                                                                      
-    while (fgets(commandQueue[i], 128, sourceCode)) {                           
+    while (fgets(commandQueue[i], LINE_MAX, sourceCode)) {                           
         commandQueue[i][strlen(commandQueue[i]) - 1] = '\0';                    
         i++;                                                                    
     }                                                                           
                
+	// Ensure that each line is synatically correct                                                                            
 	int errorLine;  
     if (containsInvalidCommand(commandQueue, numLines, &errorLine)) {	                                   
 		printf("ERROR: Invalid instruction on line %d\n", errorLine);
@@ -51,28 +67,33 @@ int main (int argc, char* argv[]) {
 	return 0;
 }            
 
-void executeCode(char commandQueue[][128], int numCommands) {
-	int memoryStack[100]; // Stack only can go 100 deep
+/**
+ * Executes a SIMPLE script.
+ * INPUT:
+ * char commandQueue[][LINE_MAX]: An array containing all commands to be executed
+ * int numCommands: Number of commands in queue
+ */
+void executeCode(char commandQueue[][LINE_MAX], int numCommands) {
+	int i, j, reti, temp;
 	int stackSize = 0;
-	int i;
-	int j;
-	int reti;
-	int temp;
-	char* tempString = malloc(sizeof(char) * 128); 
-	regex_t regex[7];
+	int memoryStack[STACK_SIZE]; // Stack only can go 100 deep
+	regex_t regex[INSTRUCTION_SET_SIZE];
 	char* validCommandRegex[] = {"PUSH [1-9][0-9]*", "POP", "ADD", 
 		"IFEQ [1-9][0-9]*", "JUMP [1-9][0-9]*", "PRINT", "DUP"};
+	char* tempString = malloc(sizeof(char) * LINE_MAX); 
 
-	for (i = 0; i < 7; i++)
+	// Compile regular expressions
+	for (i = 0; i < INSTRUCTION_SET_SIZE; i++)
 		reti = regcomp(&regex[i], validCommandRegex[i], 0);
 
+	// Loop through each instruction in the command queue
 	for (i = 0; i < numCommands; i++) 
-		for (j = 0; j < 7; j++) {
+		for (j = 0; j < INSTRUCTION_SET_SIZE; j++) { 
 			reti = regexec(&regex[j], commandQueue[i], 0, NULL, 0);
 			if (!reti) {
 				switch (j) {
-					case 0: // PUSH
-						if (stackSize >= 99) {
+					case 0: // PUSH: Adds item to top of stack
+						if (stackSize >= STACK_SIZE - 1) {
 							printf("Fatal error: Can't push to full stack.\n");
 							exit(1);
 						}
@@ -85,7 +106,7 @@ void executeCode(char commandQueue[][128], int numCommands) {
 						memoryStack[stackSize] = temp;
 						stackSize++;
 						break;
-					case 1: // POP
+					case 1: // POP: Removes item from top of stack
 						if (stackSize < 1) {
 							printf("Fatal error: Cannot pop from empty stack.\n");
 							exit(1);
@@ -93,7 +114,9 @@ void executeCode(char commandQueue[][128], int numCommands) {
 
 						stackSize--;
 						break;
-					case 2: // ADD
+					case 2: // ADD: Adds top two items on the stack together, 
+							// removes thsoe values, and places new value on 
+							// top of stack
 						if (stackSize < 2) {
 							printf("Fatal error: Missing one operand.\n");
 							exit(1);
@@ -103,26 +126,26 @@ void executeCode(char commandQueue[][128], int numCommands) {
 						stackSize--;
 						memoryStack[stackSize - 1] = temp;
 						break;
-					case 3: // IFEQ
+					case 3: // IFEQ: If top of stack is 0, jumps to another instruction
 						if (memoryStack[stackSize - 1] == 0) {
 							tempString = strcpy(tempString, commandQueue[i]);
 							strtok(tempString, " ");
 							tempString = strtok(NULL, " ");
 							temp = atoi(tempString);
-							i = temp - 2;
+							i = temp - 2; // -2 because i is incremented at end of loop
 						}		
 						break;
-					case 4: // JUMP
+					case 4: // JUMP: Jumps to a specific instruction
 						tempString = strcpy(tempString, commandQueue[i]);
 						strtok(tempString, " ");
 						tempString = strtok(NULL, " ");
 						temp = atoi(tempString);
 						i = temp - 2;
 						break; 
-					case 5: // PRINT
+					case 5: // PRINT: Outputs value of top item on stack
 						printf("%d\n", memoryStack[stackSize - 1]);
 						break;
-					case 6: // DUP
+					case 6: // DUP: Duplicates top item of the stack
 						stackSize++;
 						memoryStack[stackSize - 1] = memoryStack[stackSize - 2];
 						break;
@@ -132,7 +155,14 @@ void executeCode(char commandQueue[][128], int numCommands) {
 
 	return;
 }                                                                   
-                                                                                
+
+/**
+ * Counts the number of lines in a file.
+ * INPUT:
+ * FILE* infile: The file to count lines in
+ * RETURN VALUE:
+ * The number of lines in the file
+ */ 
 int countLines(FILE* infile) {                                                  
     int currentChar;                                                            
     int previousChar;                                                           
@@ -144,21 +174,31 @@ int countLines(FILE* infile) {
     return lineCount;                                                           
 }              
 
-int containsInvalidCommand(char commandQueue[][128], int numCommands, 
+/**
+ * Takes a list of commands and ensures they are valid.
+ * INPUT:
+ * char CommandQueue[][LINE_MAX]: A list of commands
+ * int numCommands: How many commands there are
+ * int* errorLine: A pointer to store the line number in which an error is found
+ * RETURN VALUE:
+ * Returns 0 if there are no invalid commands and 1 if there is any invalid command.
+ * Also updates the errorLine pointer, which reflects the first line where an error was found.
+ */
+int containsInvalidCommand(char commandQueue[][LINE_MAX], int numCommands, 
 	int* errorLine) {
-	int reti;
-	int flag;
-	int i;
-	regex_t regex[7];
+	int i, flag, reti;
 	char* validCommandRegex[] = {"PUSH [1-9][0-9]*", "POP", "ADD", 
 		"IFEQ [1-9][0-9]*", "JUMP [1-9][0-9]*", "PRINT", "DUP"};
+	regex_t regex[INSTRUCTION_SET_SIZE];
 
-	for (i = 0; i < 7; i++)
+	// Compile regular expressions
+	for (i = 0; i < INSTRUCTION_SET_SIZE; i++)
 		reti = regcomp(&regex[i], validCommandRegex[i], 0);
 
+	// Check each command by each regular expression to ensure all commands valid
 	for (i = 0; i < numCommands; i++) {
 		flag = 0;
-		for (int j = 0; !flag && j < 7; j++) {
+		for (int j = 0; !flag && j < INSTRUCTION_SET_SIZE; j++) {
 			reti = regexec(&regex[j], commandQueue[i], 0, NULL, 0);
 			if (!reti) 
 				flag = 1;
